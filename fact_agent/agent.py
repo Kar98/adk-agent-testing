cd8c69f371a648e94d37a298509a49a12b839398
RoryC <rory.crickmore@kablamo.com.au>
2026-01-29 16:22:31 +1100

    Using sequential

--- fact_agent/agent.py
+++ fact_agent/agent.py
@@ -20,6 +20,7 @@ MODEL_GEMINI_FLASH = "gemini-2.5-flash-lite"
 
 fact_agent = None
 validation_agent = None
+updating_agent = None
 
 known_facts = [
     "She is born in 1989",
@@ -129,24 +130,40 @@ validation_agent = Agent(
         "Use the 'validate_fact' function to judge if a fact is valid or not."\
         "If a fact is valid, then add that fact using the 'add_fact' function.",
         description="Validates any facts that are given to it.",
-        tools=[validate_fact, add_fact],
+        tools=[validate_fact],
         output_key="fact_is_valid"
     )
 
-root_agent = Agent(
-    name="root_fact_agent",
+updating_agent = Agent(
     model=MODEL_GEMINI_FLASH,
-    description="Main agent: Will leverage the sub agents to get facts about a musician.",
-    instruction="You are the main fact agent. Your job is to get facts from the fact_agent" \
-    "and then validate that fact using the validation_agent." \
-    "The flow for this is:" \
-    "1. Read the user input." \
-    "2. Get a fact from the fact_agent." \
-    "3. Validate the fact using the validation_agent." \
-    "4. If the fact is valid, then return the fact back to the user." \
-    "5. If the fact is not valid, then get a fact from 'get_safe_fact' and return that to the user." \
-    "6. Finally, call the 'log_info' function to print logging information.",
-    tools=[get_safe_fact, log_info],
-    sub_agents=[fact_agent, validation_agent],
-    output_key="lastest_fact"
+    name="validation_agent",
+    instruction=
+    "You are an agent who updates the fact list only if it's a valid fact. "\
+    "The fact is either true or false. "\
+    "If the fact is valid call the 'add_fact' function and add the fact '{fact}', then call the tool 'log_info'. If false then log info using 'log_info'. "\
+    "fact = {fact_is_valid}",
+    description="Validates any facts that are given to it.",
+    tools=[add_fact, log_info]
+)
+
+# root_agent = Agent(
+#     name="root_fact_agent",
+#     model=MODEL_GEMINI_FLASH,
+#     description="Main agent: Will leverage the sub agents to get facts about a musician.",
+#     instruction="You are the main fact agent. Your job is to get facts from the fact_agent" \
+#     "and then validate that fact using the validation_agent." \
+#     "The flow for this is:" \
+#     "1. Read the user input." \
+#     "2. Get a fact from the fact_agent." \
+#     "3. Validate the fact using the validation_agent." \
+#     "4. If the fact is valid, then return the fact back to the user." \
+#     "5. If the fact is not valid, then get a fact from 'get_safe_fact' and return that to the user." \
+#     "6. Finally, call the 'log_info' function to print logging information.",
+#     tools=[get_safe_fact, log_info],
+#     sub_agents=[fact_agent, validation_agent],
+#     output_key="lastest_fact"
+# )
+root_agent = SequentialAgent(
+    name="root_agent",
+    sub_agents=[fact_agent, validation_agent, updating_agent]
 )
